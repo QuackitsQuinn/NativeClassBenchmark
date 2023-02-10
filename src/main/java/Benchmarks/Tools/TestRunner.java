2361c12e1a527f513032796c371697bc663e6157
QuackitsQuinn <ollieollieausum@gmail.com>
2023-02-10 08:09:03 -0600

    Add Main.java and removed loadtools

--- src/main/java/Benchmarks/Tools/TestRunner.java
+++ src/main/java/Benchmarks/Tools/TestRunner.java
@@ -9,17 +9,5 @@ public class TestRunner {
         compTest.run(times);
         TestResults javaResults = compTest.getJavaResults();
         TestResults nativeResults = compTest.getNativeResults();
-        System.out.println("Java Results ============================================================");
-        System.out.println("Start Time: " + javaResults.getStartTime());
-        System.out.println("End Time: " + javaResults.getEndTime());
-        System.out.println("Elapsed Time: " + javaResults.getElapsedTime());
-        System.out.println("Run Times: " + javaResults.getRunTimes());
-        System.out.println("Average Time Per Run: " + javaResults.getAvgPerRun());
-        System.out.println("Native Results ============================================================");
-        System.out.println("Start Time: " + nativeResults.getStartTime());
-        System.out.println("End Time: " + nativeResults.getEndTime());
-        System.out.println("Elapsed Time: " + nativeResults.getElapsedTime());
-        System.out.println("Run Times: " + nativeResults.getRunTimes());
-        System.out.println("Average Time Per Run: " + nativeResults.getAvgPerRun());
     }
 }
